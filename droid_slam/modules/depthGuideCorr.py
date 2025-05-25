06c126baea0003cf0d45d1c9edc8381ddad33e16
honsen <59169297+huangliqwe2020@users.noreply.github.com>
2025-05-25 23:33:37 +0800

    Update depthGuideCorr.py

--- droid_slam/modules/depthGuideCorr.py
+++ droid_slam/modules/depthGuideCorr.py
@@ -6,7 +6,7 @@ class Attnc(nn.Module):
     def __init__(self, k_size=3):
         super(Attnc, self).__init__()
         self.avg_pool = nn.AdaptiveAvgPool2d(1)
-        self.max_pool = nn.AdaptiveAvgPool2d(1)
+        self.max_pool = nn.AdaptiveMaxPool2d(1)
         self.conv = nn.Conv1d(1, 1, kernel_size=k_size, padding=(k_size - 1) // 2, bias=False)
         self.sigmoid = nn.Sigmoid()
 
